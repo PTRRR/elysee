8944d067818d9a06a981ce9af118a1d7663468b9
Matthieu Minguet <minguet.mat@gmail.com>
2021-08-24 11:49:05 +0200

    oeuvres

---
+++ pages/collections/oeuvres.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+div
+  .infos
+    img.infos__image(src='~static/img/oeuvres.jpg')
+    .infos__content
+
+      .infos__row
+        h2.infos__title Oeuvres
+
+        .infos__content
+          .filter.active Tous
+          .filter Filtre A
+          .filter Filtre B
+          .filter Filtre C
+          .filter Filtre D
+          .filter Filtre E
+          .filter Filtre F
+          .filter Filtre G
+          br
+          br
+
+  grid(
+    :items="oeuvres"
+  )
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { oeuvres } from '@/static/content/oeuvres'
+import Grid from '@/components/Grid.vue'
+
+export default Vue.extend({
+  components: { Grid },
+
+  data() {
+    return {
+      oeuvres,
+    }
+  },
+})
+</script>
+
+<style lang="scss">
+.artwork {
+  max-width: 75%;
+  height: 75vh;
+}
+</style>
---
+++ static/content/oeuvres.js
@@ -0,0 +1,70 @@
+export const oeuvres = [
+  {
+    type: 'row',
+    blocks: [
+      {
+        img: '/img/image--1.jpg',
+      },
+      {
+        img: '/img/image--2.jpg',
+      },
+      {
+        img: '/img/image--3.jpg',
+      },
+      {
+        img: '/img/image--4.jpg',
+      },
+    ],
+  },
+  {
+    type: 'row',
+    blocks: [
+      {
+        img: '/img/image--5.jpg',
+      },
+      {
+        img: '/img/image--6.jpg',
+      },
+      {
+        img: '/img/image--7.jpg',
+      },
+      {
+        img: '/img/image--8.jpg',
+      },
+    ],
+  },
+  {
+    type: 'row',
+    blocks: [
+      {
+        img: '/img/image--9.jpg',
+      },
+      {
+        img: '/img/image--10.jpg',
+      },
+      {
+        img: '/img/image--11.jpg',
+      },
+      {
+        img: '/img/image--12.jpg',
+      },
+    ],
+  },
+  {
+    type: 'row',
+    blocks: [
+      {
+        img: '/img/image--13.jpg',
+      },
+      {
+        img: '/img/image--14.jpg',
+      },
+      {
+        img: '/img/image--3.jpg',
+      },
+      {
+        img: '/img/image--4.jpg',
+      },
+    ],
+  },
+]
